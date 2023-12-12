825c76153d8a46fe982c3c3dc6d89801d6a58a95
DFineev <fineev.mitya@yandex.ru>
2023-12-12 22:04:27 +0300

    fix change test to DataJpaTest

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -2,163 +2,167 @@ package ru.practicum.shareit.booking;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.booking.dto.BookingInfoDto;
 import ru.practicum.shareit.enums.BookingStatus;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
-import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
-@DataJpaTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@MockitoSettings(strictness = Strictness.LENIENT)
+@ExtendWith(MockitoExtension.class)
 public class BookingRepositoryTest {
-
-    @Autowired
-    @MockBean
+    @InjectMocks
     private BookingService bookingService;
-    @Autowired
-    @MockBean
-    private UserRepository userRepository;
-    @Autowired
-    private EntityManager em;
-    @Autowired
-    private BookingRepository bookingRepository;
-    private Booking booking;
 
-    private Sort sort;
+    @Mock
+    private BookingRepository bookingRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private Booking booking;
+    private User user;
 
     @BeforeEach
     void setUp() {
-        User user = User.builder()
+
+        user = User.builder()
+                .id(1)
                 .name("user")
                 .email("user@user.com")
                 .build();
 
-        User user2 = User.builder()
-                .name("user2")
-                .email("user2@user.com")
-                .build();
-
         Item item = Item.builder()
+                .id(1)
                 .name("item")
                 .description("itemDescription")
                 .available(true)
-                .owner(user2)
+                .owner(User.builder()
+                        .id(2)
+                        .build())
                 .build();
 
         booking = Booking.builder()
+                .id(1)
                 .start(LocalDateTime.now().plusHours(1))
                 .end(LocalDateTime.now().plusDays(1))
                 .item(item)
                 .booker(user)
                 .status(BookingStatus.REJECTED)
                 .build();
-        em.persist(user);
-        em.persist(user2);
-        em.persist(item);
-        em.persist(booking);
-        sort = Sort.by(Sort.Direction.ASC, "start");
     }
-
 
     @Test
     void findAllByBookerIdOrderByStartDescTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        bookingList = bookingRepository.findAllByBookerIdOrderByStartDesc(1, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByBookerIdOrderByStartDesc(anyInt(), any())).thenReturn(List.of(booking));
+        assertEquals(bookingService.getBooking(user.getId(), "ALL", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByBookerIdAndEndIsBeforeTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        LocalDateTime end = LocalDateTime.now().plusDays(1);
-        bookingList = bookingRepository.findAllByBookerIdAndEndIsBefore(1, end, sort, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByBookerIdAndEndIsBefore(anyInt(), any(), any(), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getBooking(user.getId(), "PAST", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByBookerIdAndStartIsAfterTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        LocalDateTime end = LocalDateTime.now().minusDays(1);
-        bookingList = bookingRepository.findAllByBookerIdAndStartIsAfter(1, end, sort, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByBookerIdAndStartIsAfter(anyInt(), any(), any(), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getBooking(user.getId(), "FUTURE", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByBookerIdAndStartIsBeforeAndEndIsAfterTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        bookingList = bookingRepository.findAllByBookerIdAndStartIsBeforeAndEndIsAfter(1,
-                booking.getStart().plusHours(1), booking.getEnd().minusHours(1), sort, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByBookerIdAndStartIsBeforeAndEndIsAfter(anyInt(), any(), any(), any(), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getBooking(user.getId(), "CURRENT", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByBookerIdAndStatusTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        bookingList = bookingRepository.findAllByBookerIdAndStatus(1, BookingStatus.REJECTED, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
 
-        booking.setStatus(BookingStatus.WAITING);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
 
-        bookingList = bookingRepository.findAllByBookerIdAndStatus(1, BookingStatus.WAITING, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        when(bookingRepository.findAllByBookerIdAndStatus(anyInt(), any(BookingStatus.class), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getBooking(user.getId(), "WAITING", 0, 10), List.of(bookingInfoDto));
+
+        when(bookingRepository.findAllByBookerIdAndStatus(anyInt(), any(BookingStatus.class), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getBooking(user.getId(), "REJECTED", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByItem_Owner_IdOrderByStartDescTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        bookingList = bookingRepository.findAllByItem_Owner_IdOrderByStartDesc(2, PageRequest.of(0, 10));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByItem_Owner_IdOrderByStartDesc(anyInt(), any())).thenReturn(List.of(booking));
+        assertEquals(bookingService.getOwnerBooking(user.getId(), "ALL", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByItem_Owner_IdAndEndIsBeforeTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        LocalDateTime end = LocalDateTime.now().plusDays(1);
-        bookingList = bookingRepository.findAllByItem_Owner_IdAndEndIsBefore(2, end, sort, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByItem_Owner_IdAndEndIsBefore(anyInt(), any(), any(), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getOwnerBooking(user.getId(), "PAST", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByItem_Owner_IdAndStartIsAfterTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
-        LocalDateTime end = LocalDateTime.now().minusDays(1);
-        bookingList = bookingRepository.findAllByItem_Owner_IdAndStartIsAfter(2, end, sort, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
+
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+
+        when(bookingRepository.findAllByItem_Owner_IdAndStartIsAfter(anyInt(), any(), any(), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getOwnerBooking(user.getId(), "FUTURE", 0, 10), List.of(bookingInfoDto));
+
+        when(bookingRepository.findAllByItem_Owner_IdAndStartIsBeforeAndEndIsAfter(anyInt(), any(), any(), any(), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getOwnerBooking(user.getId(), "CURRENT", 0, 10), List.of(bookingInfoDto));
     }
 
     @Test
     void findAllByItem_Owner_IdAndStatusTest() {
-        List<Booking> bookingList = new ArrayList<>();
-        assertEquals(0, bookingList.size());
+        BookingInfoDto bookingInfoDto = BookingMapper.toBookingInfoDto(booking);
 
-        bookingList = bookingRepository.findAllByItem_Owner_IdAndStatus(2, BookingStatus.REJECTED, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
 
-        booking.setStatus(BookingStatus.WAITING);
+        when(bookingRepository.findAllByItem_Owner_IdAndStatus(anyInt(), any(BookingStatus.class), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getOwnerBooking(user.getId(), "WAITING", 0, 10), List.of(bookingInfoDto));
 
-        bookingList = bookingRepository.findAllByItem_Owner_IdAndStatus(2, BookingStatus.WAITING, PageRequest.of(0, 1));
-        assertEquals(1, bookingList.size());
+        when(bookingRepository.findAllByItem_Owner_IdAndStatus(anyInt(), any(BookingStatus.class), any(PageRequest.class))).thenReturn(List.of(booking));
+        assertEquals(bookingService.getOwnerBooking(user.getId(), "REJECTED", 0, 10), List.of(bookingInfoDto));
     }
-
 }
